f16e501cac3cac37f3c2104dc8170ed308793e3a
ád ád <you@example.com>
2022-08-05 18:54:58 +0700

    abc

---
+++ module2/src/ss_01_introduction_java/thuc_hanh/show_time.java
@@ -0,0 +1,10 @@
+package ss_01_introduction_java.thuc_hanh;
+
+import java.util.Date;
+
+public class show_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("new is "+ now );
+    }
+}
